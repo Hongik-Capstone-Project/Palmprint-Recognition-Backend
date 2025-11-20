b13d39fc44191e2b70480c735bd71438c17cef54
qhloe <halfaway@yandex.com>
2025-11-20 16:09:23 +0900

    Git Test

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,29 +1,30 @@
 # app/models/__init__.py
 
 # 모든 엔티티 파일에서 핵심 클래스들을 import
+# from .auth_log import AuthLog
 from .base import Base
-from .user import User
-from .institution import Institution
-from .role import Role
-from .payment_method import PaymentMethod
 from .device import Device
+from .institution import Institution
+# from .payment_history import PaymentHistory
+from .payment_method import PaymentMethod
+from .role import Role
 from .report import Report
-from .payment_history import PaymentHistory
-from .auth_log import AuthLog
+from .user import User
 from .user_institution import UserInstitution
 from .user_institution_role import UserInstitutionRole
 
+#
 # 외부에서 'from app.models import *'를 사용할 때 노출할 클래스 지정
 __all__ = [
+    # "AuthLog",
     "Base",
-    "User",
-    "Institution",
-    "Role",
-    "PaymentMethod",
     "Device",
+    "Institution",
+    # "PaymentHistory",
+    "PaymentMethod",
+    "Role",
     "Report",
-    "PaymentHistory",
-    "AuthLog",
+    "User",
     "UserInstitution",
     "UserInstitutionRole",
 ]
